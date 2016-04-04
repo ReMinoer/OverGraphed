34693f50dba563e6d87e14ecd29e318d14d15879
Rémi Morillon <rmorillon@freneticexperience.com>
2016-04-04 20:40:00 +0200

    Remove one interface from IVisitable

--- IVisitable.cs
+++ IVisitable.cs
@@ -1,12 +1,5 @@
 ﻿namespace Diese.Graph
 {
-    public interface IVisitable<in TVisitor, TVertexBase, TEdgeBase> : IVisitable<TVisitor, TVertexBase, TVertexBase, TEdgeBase>
-        where TVisitor : IVisitor<TVertexBase, TVertexBase, TEdgeBase>
-        where TVertexBase : class, IVertex<TVertexBase, TEdgeBase>
-        where TEdgeBase : class, IEdge<TVertexBase, TEdgeBase>
-    {
-    }
-
     public interface IVisitable<in TVisitor, TVertex, TVertexBase, TEdgeBase>
         where TVisitor : IVisitor<TVertex, TVertexBase, TEdgeBase>
         where TVertex : TVertexBase
